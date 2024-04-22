f2162df99fe87a7231e3c742e13b8429cda2abdf
Aequinoxia <huangjin.china@foxmail.com>
2024-04-22 20:05:23 +0800

    update custom prompt func naming and fix some relative issue

--- prompts/all_prompts.py
+++ prompts/all_prompts.py
@@ -1,13 +1,13 @@
-def 注释(prompt):
+def comment(prompt):
     return prompt + "\n\n请你站在以英文为母语的编程专家的视角，帮我将以上内容转化成简洁、合理、地道且专业的英文注释。"
 
-def 命名(prompt):
+def naming(prompt):
     return prompt + "\n\n请你站在python编程专家的视角，为表达以上含义的python代码，提出几个简洁、合理且符合PEP 8规范的命名建议。"
 
-def 编程(prompt):
+def guide(prompt):
     return prompt + "\n\n请你站在python编程专家的视角，教我用python以尽可能简洁、合理且专业的方式实现这个需求。"
 
-def pua(prompt):
+def pro(prompt):
     prompt = "- " + prompt.replace('\n\n', '\n\n- ')
     pua_words = "Take a deep breath and tackle this problem step by step. "\
                 "If you nail it, I'll give you a hefty tip. "\
--- prompts/manager.py
+++ prompts/manager.py
@@ -7,16 +7,19 @@ class CustomPrompt:
 
     def apply_templates(self, prompt):
         """
-        自定义prompt的触发方式：在prompt开头输入自定义prompt的关键词（以空格分隔），然后中文冒号回车。
+        自定义prompt的触发方式：在prompt开头输入自定义prompt的关键词（以空格分隔），然后冒号回车。
         """
         prompt = prompt.lstrip()
-        if '：\n' in prompt:
-            trigger_content, query = prompt.split('：\n')
+        delimiter = ':\n'
+        if delimiter in prompt:
+            trigger_content, query = prompt.split(delimiter, 1)
             keywords = trigger_content.split(' ')
-        for keyword in keywords:
-            template_func = self._custom_prompts[keyword]
-            query = template_func(query)
-        return query
+            for keyword in keywords:
+                template_func = self._custom_prompts[keyword]
+                query = template_func(query)
+            return query
+        else:
+            return prompt
 
     def display_all_templates(self):
         print(', '.join(self._custom_prompts.keys()))
